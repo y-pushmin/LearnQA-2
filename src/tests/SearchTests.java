7e9f17d4201a4c483c74618ac7bdb3d654c5150b
Пушмин Ярослав Павлович <j.pushmin@cft.ru>
2021-06-08 11:23:41 +0700

    [Ex #] Практика

---
+++ src/tests/SearchTests.java
@@ -0,0 +1,52 @@
+package tests;
+
+import lib.CoreTestCase;
+import lib.ui.SearchPageObject;
+import org.junit.Test;
+
+public class SearchTests extends CoreTestCase {
+
+    @Test
+    public void testSearch()
+    {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Java");
+        SearchPageObject.waitForSearchResult("Object-oriented programming language");
+    }
+
+    @Test
+    public void testCancelSearch()
+    {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        SearchPageObject.waitForCancelButtonToAppear();
+        SearchPageObject.clickCancelSearch();
+        SearchPageObject.waitForCancelButtonToDisappear();
+    }
+
+    @Test
+    public void  testAmountOfNotEmptySearch()
+    {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        String search_line = "Linkin park discography";
+        SearchPageObject.typeSearchLine(search_line);
+        int amount_of_search_results = SearchPageObject.getAmountOfFindArticles();
+        assertTrue(
+                "We found too few results",
+                amount_of_search_results > 0
+        );
+    }
+
+    @Test
+    public void testAmountOfEmptySearch()
+    {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        String search_line = "azxcfdkfvsdv";
+        SearchPageObject.typeSearchLine(search_line);
+        SearchPageObject.waitForEmptyResultsLabel();
+        SearchPageObject.assertThereIsNoResultOfSearch();
+    }
+}
